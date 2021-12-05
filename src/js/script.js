f219cad83b0e428fd292cf2cdac115c068d15c41
Sergey <serg-nn@inbox.ru>
2021-12-05 05:20:41 +0300

    Закончил верстку desktop, сделал форму ОС

--- src/js/script.js
+++ src/js/script.js
@@ -1,72 +1,105 @@
 const slider = tns({
-    container: '.slider__inner',
-    items: 1,
-    autoplay: true,
-    speed: 1000,
-    mouseDrag: false,
-    swipeToSlide: true,
-    // responsive: {
-    //   640: {
-    //     edgePadding: 20,
-    //     gutter: 20,
-    //     items: 2
-    //   },
-    //   700: {
-    //     gutter: 30
-    //   },
-    //   900: {
-    //     items: 3
-    //   }
-    // }
-  });
-
+  container: ".slider__inner",
+  items: 1,
+  autoplay: true,
+  speed: 1000,
+  mouseDrag: false,
+  swipeToSlide: true,
+  // responsive: {
+  //   640: {
+  //     edgePadding: 20,
+  //     gutter: 20,
+  //     items: 2
+  //   },
+  //   700: {
+  //     gutter: 30
+  //   },
+  //   900: {
+  //     items: 3
+  //   }
+  // }
+});
 
 const slider1 = tns({
-    container: '.categories__slider',
-    items: 1,
-    autoplay: false,
-    speed: 1000,
-    mouseDrag: true,
-    swipeToSlide: true,
-    nav: false, //убираю точки
-    controls: false,
-    // responsive: {
-    //   640: {
-    //     edgePadding: 20,
-    //     gutter: 20,
-    //     items: 2
-    //   },
-    //   700: {
-    //     gutter: 30
-    //   },
-    //   900: {
-    //     items: 3
-    //   }
-    // }
-  });
+  container: ".categories__slider",
+  items: 1,
+  autoplay: false,
+  speed: 1000,
+  mouseDrag: false,
+  swipeToSlide: false,
+  nav: false, //убираю точки
+  controls: false,
+  // responsive: {
+  //   640: {
+  //     edgePadding: 20,
+  //     gutter: 20,
+  //     items: 2
+  //   },
+  //   700: {
+  //     gutter: 30
+  //   },
+  //   900: {
+  //     items: 3
+  //   }
+  // }
+});
 
+let indexCurrent = 1; //начинаю счетчик страниц
+let next = document.querySelector(".categories__btn_next"), //получаю кнопку вперед
+  prev = document.querySelector(".categories__btn_prev"), //получаю кнопку назад
+  page = document.querySelector(".categories__number"), // получаю элемент, куда выводить номер страницы
+  slide = document.querySelector(".categories__slider"),
+  active;
   
 
+page.innerHTML = `1 / 3`; //вывод начального состояния, загрузка с 1 страницы
 
-    let indexCurrent = 1; //начинаю счетчик страниц
-    let next = document.querySelector('.categories__btn_next'),   //получаю кнопку вперед
-    prev = document.querySelector('.categories__btn_prev'), //получаю кнопку назад
-    page = document.querySelector('.categories__number'); // получаю элемент, куда выводить номер страницы
+next.addEventListener("click", (e) => {
+  //обработчик по клику
+  e.preventDefault();
+  slider1.goTo("next"); //связываю свои кнопки с кнопками слайдера
+  indexCurrent += 1;
+  if (indexCurrent === 4) indexCurrent = 1; //допускаем переключение слайдов по кругу
+  page.innerHTML = `${indexCurrent} / 3`;
 
-    page.innerHTML = `1 / 3`; //вывод начального состояния, загрузка с 1 страницы
+  
+});
 
-    next.addEventListener('click', () => { //обработчик по клику
-        slider1.goTo('next');   //связываю свои кнопки с кнопками слайдера
-        indexCurrent+=1;
-       if (indexCurrent===4) indexCurrent=1; //допускаем переключение слайдов по кругу
-       page.innerHTML = `${indexCurrent} / 3`;
-    });
+prev.addEventListener("click", (e) => {
+  e.preventDefault();
+  slider1.goTo("prev"); //связываю свои кнопки с кнопками слайдера
+  indexCurrent -= 1;
+  if (indexCurrent === 0) indexCurrent = 3;
+  page.innerHTML = `${indexCurrent} / 3`;
+});
 
-    prev.addEventListener('click', () => {
-        slider1.goTo('prev'); //связываю свои кнопки с кнопками слайдера
-        indexCurrent-=1;
-        if (indexCurrent===0) indexCurrent=3;
-        page.innerHTML = `${indexCurrent} / 3`;
-     });
-    
-    
+slide.onclick = () => {
+  
+  setTimeout(() => {
+    active = document.querySelectorAll('.tns-slide-active');
+    console.log(active[1].id)
+  }, 2000);
+  
+}
+
+
+let btns = document.querySelectorAll('.btn'),
+    pop_up =  document.querySelector('.pop-up'),
+    close = document.querySelector('.pop-up__close');
+
+
+
+
+btns.forEach((btn) => {
+  
+    btn.addEventListener('click', () => {
+      console.log(btn.className);
+      if (btn.className!=='btn footer__btn') {
+      pop_up.classList.add('pop-up_active');
+      }
+    })
+});
+
+close.addEventListener('click', () => {
+  pop_up.classList.remove('pop-up_active');
+})
